4a59e93952ed63e412660ba68c5c816a2bfa2441
mandu <jys0972@gmail.com>
2024-07-06 12:37:21 +0900

    Create main.py
    긁어온 공지사항을 gpt로 재생성 후 게시판에 올리는 기능 구현

---
+++ Source/main.py
@@ -0,0 +1,18 @@
+from gpt_client import answer_gpt
+from crawl_announcement import crawl_anns, Announcement
+from selenium_test import WriteNoticeService
+def main():
+    anns = crawl_anns("https://cse.pusan.ac.kr/cse/14651/subview.do?enc=Zm5jdDF8QEB8JTJGYmJzJTJGY3NlJTJGMjYwNSUyRmFydGNsTGlzdC5kbyUzRmJic09wZW5XcmRTZXElM0QlMjZpc1ZpZXdNaW5lJTNEZmFsc2UlMjZzcmNoQ29sdW1uJTNEJTI2cGFnZSUzRDExJTI2c3JjaFdyZCUzRCUyNnJnc0JnbmRlU3RyJTNEJTI2YmJzQ2xTZXElM0QlMjZyZ3NFbmRkZVN0ciUzRCUyNg%3D%3D")
+
+    announcements = []
+
+    course_name = "[테스트]"
+
+    for ann in anns:
+        print("\n\n\n\n")
+        ann.content = answer_gpt(ann.get_content())
+        announcements.append(ann)
+        WriteNoticeService().write_notices(id, pw, course_name, announcements) 
+
+if __name__ == "__main__":
+    main()
