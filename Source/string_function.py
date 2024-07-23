d900408ee944458c255fe2afd2fd5f78dd4d104f
mandu <jys0972@gmail.com>
2024-07-23 20:28:19 +0900

    feat(string_function.py) : url 자르는 코드 추가

--- Source/string_function.py
+++ Source/string_function.py
@@ -3,3 +3,16 @@ def check_keywords_in_string(input_string, keywords):
         if keyword in input_string:
             return True
     return False
+
+def extract_domain_part(url, domain):
+    domain_index = url.find(domain)
+    
+    if domain_index == -1:
+        return url
+    
+    path_start_index = url.find('/', domain_index + len(domain))
+    
+    if path_start_index == -1:
+        return url
+    else:
+        return url[:path_start_index]
