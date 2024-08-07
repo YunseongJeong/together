22e596b9bdbc90071eed63d9a095ecdd21c2a0cf
mandu <jys0972@gmail.com>
2024-08-08 00:22:27 +0900

    feat(crawl_announcement.py) :  Announcement 수정

--- Source/crawl_announcement.py
+++ Source/crawl_announcement.py
@@ -4,15 +4,16 @@ from string_function import check_keywords_in_string
 import requests
 from bs4 import BeautifulSoup
 import os
+from page_url_manager import AnnouncementPage
 
 class Announcement :
-    def __init__(self, title : str,  content : str, notice_board_name:str, url : str, image_path : os.path) :
+    def __init__(self, title : str,  content : str, url : str) :
         self.title = title
         self.url = url
+        self.notice_board_name = None
         self.content = content
-        self.image_path = image_path
-        self.notice_board_name = notice_board_name
-        
+        # self.image_path = image_path
+
     def get_title(self) :
         return self.title
 
@@ -25,23 +26,6 @@ class Announcement :
     def get_image_path(self) :
         return self.image_path
 
-class AnnouncementPage:
-    def __init__(self, page_url, default_url, notice_board_name) -> None:
-        self.page_url = page_url
-        self.default_url = default_url
-        self.notice_board_name = notice_board_name
-
-class AnnouncementPages(Enum):
-    cse = AnnouncementPage(
-        page_url="https://cse.pusan.ac.kr/cse/14651/subview.do?enc=Zm5jdDF8QEB8JTJGYmJzJTJGY3NlJTJGMjYwNSUyRmFydGNsTGlzdC5kbyUzRmJic09wZW5XcmRTZXElM0QlMjZpc1ZpZXdNaW5lJTNEZmFsc2UlMjZzcmNoQ29sdW1uJTNEJTI2cGFnZSUzRDElMjZzcmNoV3JkJTNEJTI2cmdzQmduZGVTdHIlM0QlMjZiYnNDbFNlcSUzRCUyNnJnc0VuZGRlU3RyJTNEJTI2",
-        default_url="https://cse.pusan.ac.kr",
-        notice_board_name = "공모전/ 공대"
-    )
-    naoe = AnnouncementPage(
-        page_url="http://www.naoe.pusan.ac.kr/naoe/15332/subview.do",
-        default_url="http://www.naoe.pusan.ac.kr",
-        notice_board_name = "공모전/ 공대"
-    )
 
 def get_anns_url(announcementPage : AnnouncementPage) -> List[Announcement]:
     try:
@@ -81,47 +65,48 @@ def crawl_ann(url:str, annoucementPage : AnnouncementPage) -> Announcement:
     main_section = soup.find('div', "artclView")
     span_tags = main_section.find_all("span")
     article = [tag.get_text().replace("\xa0", " ") + "\n" for tag in span_tags]
-    print(article)
-    img_tags = soup.find_all('img')
-    img_path = None
-    os.makedirs('images', exist_ok=True)
-    for img_tag in img_tags:
-        img_url = img_tag['src']
-        img_name = img_url.split('/')[-1] 
-        img_path = os.path.join('images', img_name)
+    if article == []:
+        return
+    # img_tags = soup.find_all('img')
+    # img_path = None
+    # os.makedirs('images', exist_ok=True)
+    # for img_tag in img_tags:
+    #     img_url = img_tag['src']
+    #     img_name = img_url.split('/')[-1] 
+    #     img_path = os.path.join('images', img_name)
 
-        img_data = requests.get(img_url).content
-        with open(img_path, 'wb') as f:
-            f.write(img_data)
-            print(f'Saved {img_name}')
+    #     img_data = requests.get(img_url).content
+    #     with open(img_path, 'wb') as f:
+    #         f.write(img_data)
+    #         print(f'Saved {img_name}')
     
 
-    inserts = soup.find('dd', "artclInsert")
-    os.makedirs('downloads', exist_ok=True)
-    file_path = None
-    for link_tag in inserts:
-        doc_url = link_tag.find("a")
-        if (doc_url != -1):
-            href = annoucementPage.default_url + doc_url["href"]
-            file_url = href
-            file_name = doc_url.get_text(strip=True)
-            file_path = os.path.join('downloads', file_name)
-            file_data = requests.get(file_url).content
-            with open(file_path, 'wb') as f:
-                f.write(file_data)
-                print(f'Saved {file_name}')
+    # inserts = soup.find('dd', "artclInsert")
+    # os.makedirs('downloads', exist_ok=True)
+    # file_path = None
+    # for link_tag in inserts:
+    #     doc_url = link_tag.find("a")
+    #     if (doc_url != -1):
+    #         href = annoucementPage.default_url + doc_url["href"]
+    #         file_url = href
+    #         file_name = doc_url.get_text(strip=True)
+    #         file_path = os.path.join('downloads', file_name)
+    #         file_data = requests.get(file_url).content
+    #         with open(file_path, 'wb') as f:
+    #             f.write(file_data)
+    #             print(f'Saved {file_name}')
     
-    path = img_path
-    if (img_path == None) :
-        path = file_path
+    # path = img_path
+    # if (img_path == None) :
+    #     path = file_path
  
  
     return Announcement(
             title = title, 
             url = url, 
-            notice_board_name = annoucementPage.notice_board_name,
+            # notice_board_name = annoucementPage.notice_board_name,
             content = article, 
-            image_path = path
+            # image_path = path
         )
 
 
@@ -131,5 +116,7 @@ def crawl_anns(announcementPage : AnnouncementPage) :
     urls = get_anns_url(announcementPage)
     results = []
     for url in urls:
-        results.append(crawl_ann(url, announcementPage))
+        temp = crawl_ann(url, announcementPage)
+        if (temp != None):
+            results.append(temp)
     return results
